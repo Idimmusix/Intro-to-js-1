755313b3afd3aef1b6c349273dc22924f083ce3b
idimmusix <88910783+Idimmusix@users.noreply.github.com>
2022-06-22 05:56:19 +0100

    simply show my name, height and country

---
+++ script.js
@@ -0,0 +1,10 @@
+alert(
+  "My name is Idimmachukwu\nI am 1.8 meters tall\nI am from Nigeria\nWhat about you?"
+);
+const name = window.prompt("Enter your name here");
+alert("Nice to meet you " + name);
+
+// document.write(
+//   "Name: Idimmachukwu\nHeight: 1.8m\nCountry: Nigeria\n Thanks for checking in coming " +
+//     name
+// );
